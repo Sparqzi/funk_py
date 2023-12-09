8f4aac7fa115e359cd9d32f6292dd71707d0024c
Sparqzi <ekopp24@yahoo.com>
2023-12-09 16:10:44 -0600

    Adding some mostly-useless dictionaries.

---
+++ src/super_dicts/__init__.py
@@ -0,0 +1,5 @@
+from drop_none_dict import DropNoneDict      # abbr. as DnD
+from multi_key_dict import MultiKeyDict      # abbr. as MkD
+from list_dict import ListDict               # abbr. as Ld
+from multi_value_dict import MultiValueDict  # abbr. as MvD
+from windowed_list import WindowedList       # abbr. as Wl
