8b72f46e9b2bd731a7c9e611ec45a67eaefaddd8
Sparqzi <ekopp24@yahoo.com>
2023-12-27 05:53:06 -0600

    Adding in the docstring for transforming...

--- src/funk_py/modularity/decoration/__init__.py
+++ src/funk_py/modularity/decoration/__init__.py
@@ -4,4 +4,11 @@ from transforming_list import TransformingList
 
 
 def transforming(transformer: Callable):
+    """
+    Turns a class into a list that will automatically attempt to
+    transform new items to the correct type when they are appended or used to
+    extend the list.
+
+    :param transformer: A callable that will be used to transform new elements.
+    """
     return TransformingList(transformer)
