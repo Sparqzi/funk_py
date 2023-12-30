8f75477f35f1c562480757bc4b88c55f28c75f34
Sparqzi <ekopp24@yahoo.com>
2023-12-30 10:02:13 -0600

    Updated tests for DropNoneDict to include DropNoneDict.none_if.

--- test/test_drop_none_dict.py
+++ test/test_drop_none_dict.py
@@ -1,8 +1,9 @@
-from typing import Tuple, Any
+from collections import namedtuple
+from enum import IntFlag
+from typing import Tuple, Any, Union
 
 import pytest
 from funk_py.super_dicts.drop_none_dict import DropNoneDict as DnD
-
 
 NOT_CAUSE = 'Test failed to generate the expected start value. The issue is' \
             ' probably elsewhere.'
@@ -158,3 +159,206 @@ def test_del_keys(regular_values_base):
         del dnd[key]
         del copied[key]
         assert dnd == copied
+
+
+# ------------------------------------------------------------------------------
+# none_if tests
+# ------------------------------------------------------------------------------
+
+NoneIfTst = namedtuple('NoneIfTst',
+                       {'value', 'none_val', 'plug_in', 'plug_out', 'output'})
+
+GV = 19
+LV = GV - 1
+LLV = GV - 2
+GENERAL_PLUG_IN = lambda x: x + 1  # noqa
+DIFFERENT_PLUG_IN = lambda x: x < GV  # noqa
+GENERAL_PLUG_OUT = 'same_type_plug_out'
+DIFFERENT_PLUG_OUT = 'different_type_plug_out'
+
+
+def none_if_dicts(tst: NoneIfTst, values_base: dict):
+    # This test is a little overkill for none_if's that actually end up being
+    # None, but so be it.
+    m_key = K[len(V)]
+    none_if = DnD.none_if(tst.value, tst.none_val, plug_in=tst.plug_in,
+                          plug_out=tst.plug_out)
+
+    # Test case to ensure if none_if is the first value, position is maintained.
+    input1 = {m_key: none_if}
+    input1.update(values_base)
+    out1 = {m_key: tst.output} if tst.output is not None else {}
+    out1.update(values_base)
+
+    # Test case to ensure if none_if is neither the first nor last value,
+    # position is maintained.
+    bi = iter(values_base.items())
+    k, v = next(bi)
+    input2 = {k: v, m_key: none_if}
+    out2 = {k: v}
+    if tst.output is not None: out2[m_key] = tst.output  # noqa
+
+    for k, v in bi:
+        input2[k] = v
+        out2[k] = v
+
+    input3 = values_base.copy()
+    input3[m_key] = none_if
+    out3 = values_base.copy()
+    if tst.output is not None: out3[m_key] = tst.output  # noqa
+
+    return [(input1, out1), (input2, out2), (input3, out3)]
+
+
+@pytest.fixture(params=[(LV, LV), (GV, None)],
+                ids=['value != none_val', 'value == none_val'])
+def simple_none_if_args(request):
+    value, output = request.param
+    return NoneIfTst(value=value, none_val=GV, plug_in=..., plug_out=...,
+                     output=output)
+
+
+@pytest.fixture(params=[
+    (LLV, LV, GENERAL_PLUG_IN),
+    (GV, None, GENERAL_PLUG_IN),
+    (LV, GV, GENERAL_PLUG_IN),
+    (LV, True, DIFFERENT_PLUG_IN),
+    (GV, None, DIFFERENT_PLUG_IN)
+], ids=['value != none_val',
+        'value == none_val',
+        "value != none_val (Doesn't Double Filter)",
+        'value != none_val (Diff Type)',
+        'value == none_val (Diff Type)'])
+def plug_in_none_if_args(request):
+    value, output, plug_in = request.param
+    return NoneIfTst(value=value, none_val=GV, plug_in=plug_in, plug_out=...,
+                     output=output)
+
+
+@pytest.fixture(params=[
+    (LLV, LV, (..., int), GENERAL_PLUG_OUT),
+    (GV, None, (..., int), GENERAL_PLUG_OUT),
+    (LV, GV, (..., int), GENERAL_PLUG_OUT),
+    (LV, GV, (..., ...), GENERAL_PLUG_OUT),
+    (GV, None, (..., ...), GENERAL_PLUG_OUT),
+    (LV, True, (..., int), DIFFERENT_PLUG_OUT),
+    (GV, None, (..., int), DIFFERENT_PLUG_OUT),
+    (LV, True, (..., ...), DIFFERENT_PLUG_OUT),
+    (GV, None, (..., ...), DIFFERENT_PLUG_OUT)
+], ids=[
+    'value != none_val (ValidPlugOut/int)',
+    'value == none_val (ValidPlugOut/int)',
+    "value != none_val (ValidPlugOut/int) (Doesn't Double Filter)",
+    'value != none_val (ValidPlugOut/ValidPlugOut)',
+    'value == none_val (ValidPlugOut/ValidPlugOut)',
+    'value != none_val (ValidPlugOut/int) (Diff Type)',
+    'value == none_val (ValidPlugOut/int) (Diff Type)',
+    'value != none_val (ValidPlugOut/ValidPlugOut) (Diff Type)',
+    'value == none_val (ValidPlugOut/ValidPlugOut) (Diff Type)'
+])
+def plug_out_none_if_args(request):
+    value, output, types, plug_out = request.param
+
+    class HasValidPlugOut:
+        def __init__(self, value: int):
+            self.value = value
+
+        def __eq__(self, other: Union['HasValidPlugOut', int]) -> bool:
+            if type(other) is int:
+                return self.value == other
+
+            return self.value == other.value
+
+        def same_type_plug_out(self) -> int:
+            return self.value + 1
+
+        def different_type_plug_out(self) -> bool:
+            return self.value < GV
+
+    none_val = GV if types[1] is int else HasValidPlugOut(GV)
+    if types[0] is not int:
+        value = HasValidPlugOut(value)
+
+    return NoneIfTst(value=value, none_val=none_val, plug_in=...,
+                     plug_out=plug_out, output=output)
+
+
+def test_none_if_later_creates_outside_of_dict_simple(simple_none_if_args):
+    testy = DnD.none_if(simple_none_if_args.value, simple_none_if_args.none_val)
+    assert isinstance(testy, DnD._NoneIfLater)
+    assert testy.none_val == simple_none_if_args.none_val
+    assert testy.value == simple_none_if_args.value
+    assert testy.plug_in is ..., 'plug_in was not empty!'
+    assert testy.plug_out is ..., 'plug_out was not empty!'
+
+
+def test_none_if_later_creates_outside_of_dict_plug_in(plug_in_none_if_args):
+    testy = DnD.none_if(plug_in_none_if_args.value,
+                        plug_in_none_if_args.none_val,
+                        plug_in=plug_in_none_if_args.plug_in)
+    assert isinstance(testy, DnD._NoneIfLater)
+    assert testy.none_val == plug_in_none_if_args.none_val
+    assert testy.value == plug_in_none_if_args.value
+    assert testy.plug_in is plug_in_none_if_args.plug_in
+    assert testy.plug_out is ..., 'plug_out was not empty!'
+
+
+def test_none_if_later_creates_outside_of_dict_plug_out(plug_out_none_if_args):
+    testy = DnD.none_if(plug_out_none_if_args.value,
+                        plug_out_none_if_args.none_val,
+                        plug_out=plug_out_none_if_args.plug_out)
+    assert isinstance(testy, DnD._NoneIfLater)
+    assert testy.none_val == plug_out_none_if_args.none_val
+    assert testy.value == plug_out_none_if_args.value
+    assert testy.plug_in is ..., 'plug_in was not empty!'
+    assert testy.plug_out is plug_out_none_if_args.plug_out
+
+
+VF1 = 'none_if as first value failed!'
+VF2 = 'none_if as second value failed!'
+VF3 = 'none_if as last value failed!'
+
+
+def test_none_if_later_works_in_dict_simple(simple_none_if_args,
+                                            regular_values_base):
+    tests = none_if_dicts(simple_none_if_args, regular_values_base)
+
+    testy = DnD(tests[0][0])
+    assert testy == tests[0][1], VF1
+
+    testy = DnD(tests[1][0])
+    assert testy == tests[1][1], VF2
+
+    testy = DnD(tests[2][0])
+    assert testy == tests[2][1], VF3
+
+
+def test_none_if_later_works_in_dict_plug_in(plug_in_none_if_args,
+                                             regular_values_base):
+    tests = none_if_dicts(plug_in_none_if_args, regular_values_base)
+
+    testy = DnD(tests[0][0])
+    assert testy == tests[0][1], VF1
+
+    testy = DnD(tests[1][0])
+    assert testy == tests[1][1], VF2
+
+    testy = DnD(tests[2][0])
+    assert testy == tests[2][1], VF3
+
+
+def test_none_if_later_works_in_dict_plug_out(plug_out_none_if_args,
+                                              regular_values_base):
+    tests = none_if_dicts(plug_out_none_if_args, regular_values_base)
+
+    testy = DnD(tests[0][0])
+    assert testy == tests[0][1], VF1
+
+    testy = DnD(tests[1][0])
+    assert testy == tests[1][1], VF2
+
+    testy = DnD(tests[2][0])
+    assert testy == tests[2][1], VF3
+
+
+
