e5d5e08c3991403c15abfdbb27209eb8fe84f9ad
Sparqzi <ekopp24@yahoo.com>
2024-04-13 17:48:23 -0500

    Adding more tests and altering structure of pick function.

--- src/funk_py/sorting/pieces.py
+++ src/funk_py/sorting/pieces.py
@@ -3,6 +3,8 @@ import xml.etree.ElementTree as ET
 import json
 import csv
 from enum import IntEnum
+from typing import Mapping, Any, Literal, Union, List, Dict, Tuple, Optional, Iterator, Generator, \
+    Callable
 
 import yaml
 from typing import Mapping, Any, Literal, Union, List, Dict
@@ -12,70 +14,166 @@ class PickType(IntEnum):
     MULTIPLIED = 0
     ADDITIVE = 1
     NESTED = 2
+    MULTIPLICATIVE = 0
+    AGGREGATION = 1
 
 
 def pick(
-        output_map: Union[Mapping, List[Union[Literal['json', 'xml', 'xml-sa', 'e-list', 'list',
-                                                      'csv', 'yaml'], str, Mapping]]],
+        output_map: OutputMapType,
         _input: Any,
-        list_handling_method: PickType = PickType.MULTIPLIED) -> list:
-    builder = []
-    static_builder = {}
+        list_handling_method: PickType = PickType.MULTIPLICATIVE
+) -> list:
+    if list_handling_method == PickType.MULTIPLICATIVE:
+        return _pick_multiplicative(output_map, _input)
 
+
+def _pick_setup(
+        output_map: OutputMapType,
+        _input: Any
+) -> Tuple[Optional[OutputMapType], Any, list, bool]:
     if isinstance(output_map, list):
-        worker = parse_type_as(output_map[0], _input)
-        output_map = output_map[1]
+        # If the output_map is a list, that should mean the user specified a type of conversion to
+        # enact on the input (as long as their output_map is valid).
+        if isinstance(_input, list) and output_map[0] != 'e-list':
+            # In the case that _input is a list, we should first check if the user specified e-list
+            # since that means "expected list". If they did specify that, we'll want to consume it,
+            # so this branch won't execute, and we'll move over to the next step. If they didn't, we
+            # continue on our mary way, and don't move forward on the output_map yet. We want to
+            # pass the non-mutated _input as well since there's no mutation to be performed on it.
+            return output_map, _input, [], False
+
+        else:
+            # In the case that _input is not a list, we will just attempt to parse as the user
+            # expected.
+            try:
+                worker = parse_type_as(output_map[0], _input)
+
+            except Exception as e:
+                main_logger.warning(f'User\'s expected parsing method failed. Exception raised: '
+                                    f'{e}')
+                return None, None, [], True
+
+            return output_map[1], worker, [], False
 
     elif isinstance(output_map, str):
-        builder.append({output_map: _input})
-        return builder
+        # Theoretically, we should never get here, but just in case, this line should catch
+        # instances where a key is immediately requested. Who knows, maybe a change down the line
+        # will cause this code to be needed.
+        return None, None, [{output_map: _input}], False
+
+    # If nothing caught, just pass everything back as-is.
+    return output_map, _input, [], False
+
+
+def _find_and_follow_first_path_in_pick(
+        output_map_iter: Iterator,
+        worker: Any,
+        builder: list,
+        static_builder: dict
+) -> Tuple[Optional[OutputMapType], Any, bool]:
+    while True:
+        try:
+            path, instruction = next(output_map_iter)
+            if path in worker:
+                if isinstance(instruction, str):
+                    if not isinstance(worker, dict):
+                        main_logger.warning(f'An unexpected value was encountered in pick attempt. '
+                                            f'a path has now been skipped. value = {worker}')
+                        return None, None, False
+
+                    static_builder[instruction] = worker[path]
+                    builder.append({})
+                    # The caller doesn't need to call anything, we did everything needed for the
+                    # first key.
+                    return None, None, False
+
+                else:
+                    # Rather than recursively calling a function from inside of this function, we
+                    # pass back the values that the caller needs to use to call the function itself.
+                    # This is done this way to limit how deep we go on the stack. It also means the
+                    # caller can decide which function to call on the values.
+                    return instruction, worker[path], False
+
+        except StopIteration:
+            # Welp, the caller will have to return immediately, none of the paths requested were
+            # present.
+            return None, None, True
+
+
+def _pick_iter(
+        output_map_iter: Iterator,
+        worker: Any,
+        builder: list,
+        static_builder: dict,
+        func: Callable[[list, list, dict], None]
+) -> Generator[Tuple[Optional[OutputMapType], Any, bool], list, None]:
+    """
+    Finishes iterating after the first path was successfully followed. Expects the result of an
+    external function call to be sent to it if it yields an instruction.
+    """
+    for path, instruction in output_map_iter:
+        if path in worker:
+            if isinstance(instruction, str):
+                static_builder[instruction] = worker[path]
+
+            else:
+                # Rather than recursively calling a function from inside of this function, we
+                # pass back the values that the caller needs to use to call the function itself.
+                # This is done this way to limit how deep we go on the stack. It also means the
+                # caller can decide which function to call on the values.
+                # The caller should send the result of the function back to us here so that we can
+                # call func on it.
+                result = yield instruction, worker[path], False
+                func(result, builder, static_builder)
+
+    yield None, None, True
+
+
+def _pick_multiplicative_func(ans: list, builder: list, static_builder: dict) -> None:
+    if len(ans) != 1:
+        for i in range(len(builder)):
+            for _ans in ans[1:]:
+                copier = builder[i].copy()
+                copier.update(_ans)
+                builder.append(copier)
+
+            builder[i].update(ans[0])
 
     else:
-        worker = _input
+        for source in builder:
+            source.update(ans[0])
+
+
+def _pick_multiplicative(output_map: OutputMapType, _input: Any) -> list:
+    static_builder = {}
+    output_map, worker, builder, fail = _pick_setup(output_map, _input)
+    if fail or len(builder):
+        return builder
 
     if isinstance(worker, list):
         for item in worker:
-            builder.extend(pick(output_map, item))
+            builder.extend(_pick_multiplicative(output_map, item))
 
     else:
         output_map_iter = iter(output_map.items())
+        *instruction, return_now = _find_and_follow_first_path_in_pick(
+            output_map_iter, worker, builder, static_builder)
+        if return_now:
+            return builder
 
-        not_found = True
-        while not_found:
-            try:
-                path, instruction = next(output_map_iter)
-                if path in worker:
-                    if isinstance(instruction, str):
-                        static_builder[instruction] = worker[path]
-                        builder.append({})
+        elif instruction[0] is not None:
+            # If instructions were given by _find_and_follow_first_path_in_pick, then we need to
+            # recursively call this function with the results.
+            builder.extend(_pick_multiplicative(*instruction))
 
-                    else:
-                        builder.extend(pick(instruction, worker[path]))
+        pick_iter = _pick_iter(
+            output_map_iter, worker, builder, static_builder, _pick_multiplicative_func)
 
-                    not_found = False
-
-            except StopIteration:
-                return []
-
-        for path, instruction in output_map_iter:
-            if path in worker:
-                if isinstance(instruction, str):
-                    static_builder[instruction] = worker[path]
-
-                else:
-                    ans = pick(instruction, worker[path])
-                    if len(ans) != 1:
-                        for i in range(len(builder)):
-                            for _ans in ans[1:]:
-                                copier = builder[i].copy()
-                                copier.update(_ans)
-                                builder.append(copier)
-
-                            builder[i].update(ans[0])
-
-                    else:
-                        for source in builder:
-                            source.update(ans[0])
+        for *instruction, return_now in pick_iter:
+            if instruction[0] is not None:
+                # If instructions were given by pick_iter, then we need to recursively call this
+                # function and pass the results back to pick_iter.
+                pick_iter.send(_pick_multiplicative(*instruction))
 
         for result in builder:
             result.update(static_builder)
--- test/test_pick.py
+++ test/test_pick.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from collections import namedtuple
 
 import yaml
@@ -9,8 +10,8 @@ import pytest
 
 from funk_py.sorting.pieces import pick
 
-KEYS = ['k1', 'k2', 'k3', 'k4', 'k5', 'k6']
-OUT_KEYS = ['o1', 'o2', 'o3', 'o4', 'o5', 'o6']
+KEYS = ['k' + str(i) for i in range(100)]
+OUT_KEYS = ['o' + str(i) for i in range(100)]
 VALS1 = ['llama', 'horse', 'bear']
 VALS2 = ['Hope', 'Jeffrey', 'Maldo']
 VALS3 = ['funny', 'scary', 'happy']
@@ -20,6 +21,12 @@ VALS6 = ['lovely', 'strange', 'terrifying']
 DATA_KEY1 = 'data'
 DATA_KEY2 = 'group'
 TEXT = 'text'
+
+
+def compare_lists_of_dicts_unordered(list1, list2):
+    sorted1 = sorted(list1, key=lambda d: sorted(d.items()))
+    sorted2 = sorted(list2, key=lambda d: sorted(d.items()))
+    assert sorted1 == sorted2
 
 
 def make_csv(keys: List[str], *val_sets: list) -> str:
@@ -66,11 +73,18 @@ def make_jsonl_list(keys: List[str], *val_sets: list) -> str:
     return '\n'.join(json.dumps(group) for group in source)
 
 
+def safe_json(value):
+    if isinstance(value, list) or isinstance(value, dict):
+        return json.dumps(value)
+
+    return str(value)
+
+
 def make_xml_dict_no_attr(keys: List[str], *vals: Any) -> str:
     builder = ET.Element(DATA_KEY2)
     for i in range(len(keys)):
         cur = ET.SubElement(builder, keys[i])
-        cur.text = str(vals[i])
+        cur.text = safe_json(vals[i])
 
     return ET.tostring(builder, 'unicode')
 
@@ -87,7 +101,7 @@ def make_xml_list_no_attr(keys: List[str], *val_sets: list) -> str:
 
 
 def make_xml_dict_attr(keys: List[str], *vals: Any) -> str:
-    builder = ET.Element(DATA_KEY2, make_json_dict(keys, *vals))
+    builder = ET.Element(DATA_KEY2, make_json_dict(keys, *[safe_json(v) for v in vals]))
     return ET.tostring(builder, 'unicode')
 
 
@@ -107,10 +121,8 @@ def make_yaml_list(keys: List[str], *val_sets: list) -> str:
     return yaml.dump(make_json_list(keys, *val_sets))
 
 
-ListSet = namedtuple(
-    'ListSet',
-    ('list1', 'list2', 'instruction', 'result1', 'result2')
-)
+ListSet = namedtuple('ListSet', ('list1', 'list2', 'instruction', 'result1', 'result2'))
+DictSet = namedtuple('DictSet', ('dict', 'instruction', 'result'))
 
 
 @pytest.fixture(params=(
@@ -132,7 +144,7 @@ ListSet = namedtuple(
     'spacy csv lists',
     'yaml lists',
 ))
-def lists(request):
+def similar_lists(request):
     func, instruction = request.param
     list1 = func(KEYS[:3], VALS1, VALS2, VALS3)
     list2 = func(KEYS[:3], VALS4, VALS5, VALS6)
@@ -151,6 +163,48 @@ def lists(request):
 
 
 @pytest.fixture(params=(
+    (make_json_list, None),
+    (make_json_list_str, 'json'),
+    (make_jsonl_list, 'jsonl'),
+    (make_xml_list_attr, 'xml'),
+    (make_xml_list_no_attr, 'xml-sa'),
+    (make_csv, 'csv'),
+    (make_spacy_csv, 'csv'),
+    (make_yaml_list, 'yaml'),
+), ids=(
+    'regular lists',
+    'json string lists',
+    'jsonl string',
+    'xml with attributes lists',
+    'xml without attributes lists',
+    'csv lists',
+    'spacy csv lists',
+    'yaml lists',
+))
+def dissimilar_lists(request):
+    func, instruction = request.param
+    list1 = func(KEYS[:3], VALS1, VALS2, VALS3)
+    list2 = func(KEYS[3:6], VALS4, VALS5, VALS6)
+    _instruction1 = dict(zip(KEYS[:3], OUT_KEYS[:3]))
+    _instruction2 = dict(zip(KEYS[3:6], OUT_KEYS[3:6]))
+    if instruction is not None:
+        if instruction in ('xml', 'xml-sa'):
+            _instruction1 = [instruction, {DATA_KEY1: {DATA_KEY2: _instruction1}}]
+            _instruction2 = [instruction, {DATA_KEY1: {DATA_KEY2: _instruction2}}]
+
+        else:
+            _instruction1 = [instruction, _instruction1]
+            _instruction2 = [instruction, _instruction2]
+
+    _instruction = [_instruction1, _instruction2]
+
+    result1 = make_json_list(OUT_KEYS[:3], VALS1, VALS2, VALS3)
+    result2 = make_json_list(OUT_KEYS[3:6], VALS4, VALS5, VALS6)
+
+    return ListSet(list1, list2, _instruction, result1, result2)
+
+
+@pytest.fixture(params=(
     (make_json_dict, None),
     (make_json_dict_str, 'json'),
     (make_xml_dict_attr, 'xml'),
@@ -163,15 +217,15 @@ def lists(request):
     'xml without attributes dict',
     'yaml dict',
 ))
-def dicts_with_one_nested_list(request, lists):
+def dicts_with_one_nested_list(request, similar_lists):
     func, instruction = request.param
-    dict1 = func([KEYS[3]], lists.list1)
-    dict2 = func([KEYS[3]], lists.list2)
-    _instruction = lists.instruction
+    dict1 = func([KEYS[3]], similar_lists.list1)
+    dict2 = func([KEYS[3]], similar_lists.list2)
+    _instruction = similar_lists.instruction
     if instruction is not None:
         if instruction in ('xml', 'xml-sa'):
-            if type(lists.instruction) is dict:
-                _instruction = ['json\'', _instruction]
+            if type(similar_lists.instruction) is dict:
+                _instruction = ['json', _instruction]
 
             _instruction = [instruction, {DATA_KEY2: {KEYS[3]: _instruction}}]
 
@@ -181,23 +235,173 @@ def dicts_with_one_nested_list(request, lists):
     else:
         _instruction = {KEYS[3]: _instruction}
 
-    return ListSet(dict1, dict2, _instruction, lists.result1, lists.result2)
+    return ListSet(dict1, dict2, _instruction, similar_lists.result1, similar_lists.result2)
 
 
-class TestLists:
-    def test_simple_lists_multiplied(self, lists):
-        ans = pick(lists.instruction, lists.list1)
-        assert ans == lists.result1
+@pytest.fixture(params=(
+    (make_json_dict, None),
+    (make_json_dict_str, 'json'),
+    (make_xml_dict_attr, 'xml'),
+    (make_xml_dict_no_attr, 'xml-sa'),
+    (make_yaml_dict, 'yaml'),
+), ids=(
+    'regular dict',
+    'json string dict',
+    'xml with attributes dict',
+    'xml without attributes dict',
+    'yaml dict',
+))
+def dict_with_two_nested_similar_lists_in_list(request, similar_lists):
+    func, instruction = request.param
+    _dict = func([KEYS[6]], [similar_lists.list1, similar_lists.list2])
+    _instruction = similar_lists.instruction
+    if instruction is not None:
+        if instruction in ('xml', 'xml-sa'):
+            _instruction = [instruction, {DATA_KEY2: {KEYS[6]: ['json', _instruction]}}]
 
-        ans = pick(lists.instruction, lists.list2)
-        assert ans == lists.result2
+        else:
+            _instruction = [instruction, {KEYS[6]: _instruction}]
 
-    def test_single_dict_nested_lists_multiplied(self, dicts_with_one_nested_list):
-        print(repr(dicts_with_one_nested_list.list1))
-        print(repr(dicts_with_one_nested_list.list2))
-        print(repr(dicts_with_one_nested_list.instruction))
+    else:
+        _instruction = {KEYS[6]: _instruction}
+
+    return DictSet(_dict, _instruction, similar_lists.result1 + similar_lists.result2)
+
+
+@pytest.fixture(params=(
+    (make_json_dict, None),
+    (make_json_dict_str, 'json'),
+    (make_xml_dict_attr, 'xml'),
+    (make_xml_dict_no_attr, 'xml-sa'),
+    (make_yaml_dict, 'yaml'),
+), ids=(
+    'regular dict',
+    'json string dict',
+    'xml with attributes dict',
+    'xml without attributes dict',
+    'yaml dict',
+))
+def dict_with_two_nested_dissimilar_lists_in_list(request, dissimilar_lists):
+    func, instruction = request.param
+    _dict = func([KEYS[6]], [dissimilar_lists.list1, dissimilar_lists.list2])
+    _instruction = make_json_dict(KEYS[:6], *OUT_KEYS[:6])
+    if isinstance(dissimilar_lists.instruction[0], list):
+        if (t := dissimilar_lists.instruction[0][0]) in ('xml', 'xml-sa'):
+            _instruction = [t, {DATA_KEY1: {DATA_KEY2: _instruction}}]
+
+        else:
+            _instruction = [t, _instruction]
+
+    if instruction is not None:
+        if instruction in ('xml', 'xml-sa'):
+            _instruction = [
+                instruction,
+                {
+                    DATA_KEY2: {
+                        KEYS[6]: [
+                            'json',
+                            _instruction
+                        ],
+                    },
+                },
+            ]
+
+        else:
+            _instruction = [instruction, {KEYS[6]: _instruction}]
+
+    else:
+        _instruction = {KEYS[6]: _instruction}
+
+    return DictSet(_dict, _instruction, dissimilar_lists.result1 + dissimilar_lists.result2)
+
+
+@pytest.fixture(params=(
+        (make_json_dict, None),
+        (make_json_dict_str, 'json'),
+        (make_xml_dict_attr, 'xml'),
+        (make_xml_dict_no_attr, 'xml-sa'),
+        (make_yaml_dict, 'yaml'),
+), ids=(
+        'regular dict',
+        'json string dict',
+        'xml with attributes dict',
+        'xml without attributes dict',
+        'yaml dict',
+))
+def two_nested_lists_under_keys(request, dissimilar_lists):
+    func, instruction = request.param
+    _dict = func(KEYS[6:8], dissimilar_lists.list1, dissimilar_lists.list2)
+    _instruction1, _instruction2 = dissimilar_lists.instruction
+    t = None
+
+    if instruction is not None:
+        if instruction in ('xml', 'xml-sa'):
+            if isinstance(_instruction1, dict):
+                _instruction = [
+                    instruction,
+                    {
+                        DATA_KEY2: {
+                            KEYS[6]: ['json', _instruction1],
+                            KEYS[7]: ['json', _instruction2],
+                        },
+                    },
+                ]
+
+            else:
+                _instruction = [
+                    instruction,
+                    {
+                        DATA_KEY2: {
+                            KEYS[6]: _instruction1,
+                            KEYS[7]: _instruction2,
+                        },
+                    },
+                ]
+
+        else:
+            _instruction = [instruction, {KEYS[6]: _instruction1, KEYS[7]: _instruction2}]
+
+    else:
+        _instruction = {KEYS[6]: _instruction1, KEYS[7]: _instruction2}
+
+    result = []
+    for result1 in dissimilar_lists.result1:
+        for result2 in dissimilar_lists.result2:
+            copier = result1.copy()
+            copier.update(result2)
+            result.append(copier)
+
+    return DictSet(_dict, _instruction, result)
+
+
+class TestMultiplicative:
+    def test_simple_lists(self, similar_lists):
+        ans = pick(similar_lists.instruction, similar_lists.list1)
+        assert ans == similar_lists.result1
+
+        ans = pick(similar_lists.instruction, similar_lists.list2)
+        assert ans == similar_lists.result2
+
+    def test_single_dict_nested_lists(self, dicts_with_one_nested_list):
         ans = pick(dicts_with_one_nested_list.instruction, dicts_with_one_nested_list.list1)
         assert ans == dicts_with_one_nested_list.result1
 
         ans = pick(dicts_with_one_nested_list.instruction, dicts_with_one_nested_list.list2)
         assert ans == dicts_with_one_nested_list.result2
+
+    def test_dict_nested_lists_in_list(self, dict_with_two_nested_similar_lists_in_list):
+        ans = pick(dict_with_two_nested_similar_lists_in_list.instruction,
+                   dict_with_two_nested_similar_lists_in_list.dict)
+        assert ans == dict_with_two_nested_similar_lists_in_list.result
+
+    def test_dict_nested_dissimilar_lists_in_list(
+            self,
+            dict_with_two_nested_dissimilar_lists_in_list
+    ):
+        ans = pick(dict_with_two_nested_dissimilar_lists_in_list.instruction,
+                   dict_with_two_nested_dissimilar_lists_in_list.dict)
+        assert ans == dict_with_two_nested_dissimilar_lists_in_list.result
+
+    def test_dict_nested_dissimilar_lists_under_keys(self, two_nested_lists_under_keys):
+        ans = pick(two_nested_lists_under_keys.instruction, two_nested_lists_under_keys.dict)
+        compare_lists_of_dicts_unordered(ans, two_nested_lists_under_keys.result)
